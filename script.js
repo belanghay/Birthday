683b94a3142da0a05227beb5fb2669b61d9057d7
belanghay <113161874+belanghay@users.noreply.github.com>
2026-09-04 21:39:25 +0800

    Change birthday message to include emoji
    Updated birthday message to include a heart emoji.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Belated Happy Birthday Theressa').split('')
+      const titles = ('Belated Happy Birthday Theressa ❤️').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
